1516c39bccb962a57a0cebae096ef718c4623962
Paulo Henrique <paulopradella@outlook.com>
2021-05-22 12:43:19 -0300

    Programa para gerar uma pirâmide retirado do instagram pythn.hub para fins de estudo

---
+++ piramide/Piramide.py
@@ -0,0 +1,20 @@
+#retirado do instagram @python.hub para fins de estudo
+
+fila = 5
+
+k = 0
+
+for i in range(1, fila+1):
+
+    for espaco in range(1, (fila-i)+1):
+        print(end="  ")
+
+    while k != (2*i-1):
+
+        print("* ", end = " ")
+
+        k += 1
+
+    k = 0
+
+    print()
